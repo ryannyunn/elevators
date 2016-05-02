72764e4213cda410e0b9ff954ebfd17128afb047
ryannyunn <ryannyunn@gmail.com>
2016-05-02 18:46:49 -0400

    goodbye heatmap

--- js/maps.js
+++ js/maps.js
@@ -1,28 +1,28 @@
 var styles = [{"featureType":"administrative","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"administrative.country","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"administrative.province","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#e3e3e3"}]},{"featureType":"landscape.natural","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"color":"#cccccc"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"transit.station.airport","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"transit.station.airport","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#FFFFFF"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]}];
 
-function heatmap(elevators) {
-  var initialize = function() {
-    var mapOptions = {
-      zoom: 11,
-      center: new google.maps.LatLng(40.7293, -73.9906),
-      scrollwheel: false,
-      streetViewControl: false
-    };
-    var map = new google.maps.Map(document.getElementById('heatMap'), mapOptions);
-    var positions = [];
-    for (i = 1; i < elevators.length; i++) {
-      positions.push(new google.maps.LatLng(elevators[i][27], elevators[i][28]));
-    }
-    var heatmap = new google.maps.visualization.HeatmapLayer({
-      data: positions,
-      map: map
-    });
-    map.setOptions({styles: styles});
-    heatmap.setMap(map);
-    heatmap.set('radius', 150);
-  }
-  google.maps.event.addDomListener(window, 'load', initialize);
-}
+//function heatmap(elevators) {
+//  var initialize = function() {
+//    var mapOptions = {
+//      zoom: 11,
+//      center: new google.maps.LatLng(40.7293, -73.9906),
+//      scrollwheel: false,
+//      streetViewControl: false
+//    };
+//    var map = new google.maps.Map(document.getElementById('heatMap'), mapOptions);
+//    var positions = [];
+//    for (i = 1; i < elevators.length; i++) {
+//      positions.push(new google.maps.LatLng(elevators[i][27], elevators[i][28]));
+//    }
+//    var heatmap = new google.maps.visualization.HeatmapLayer({
+//      data: positions,
+//      map: map
+//    });
+//    map.setOptions({styles: styles});
+//    heatmap.setMap(map);
+//    heatmap.set('radius', 150);
+//  }
+//  google.maps.event.addDomListener(window, 'load', initialize);
+//}
 
 function markermap(elevators) {
   function initialize() {
@@ -46,7 +46,7 @@ function markermap(elevators) {
 
     layer = new google.maps.FusionTablesLayer({
       map: map,
-      heatmap: { enabled: true },
+      heatmap: { enabled: false },
       query: {
         select: "col26",
         from: "12PsvIPMRRm3sX-Q5kjIaCOyLOkPggzXrVZY79WLM",
